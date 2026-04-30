e22c511a1d1960f5f9e658c4abdc50ad69bd8fc8
NU HowToPay <nu@htpdesktop.com>
2026-04-30 21:41:28 +0700

    UI: Add faction colors to card borders

--- js/cards.js
+++ js/cards.js
@@ -8,6 +8,7 @@ const CardDatabase = {
             atkCost: 2, 
             skills: ["rush"], 
             type: "warrior",
+            faction: "china",
             img: "https://res.cloudinary.com/dju44op8w/image/upload/v1777467648/Gemini_Generated_Image_4wo9z44wo9z44wo9_ry0w3f.jpg" 
         },
         { 
@@ -15,7 +16,8 @@ const CardDatabase = {
             atk: 6500, 
             cost: 4, 
             atkCost: 1, 
-            type: "blocker", 
+            type: "blocker",
+            faction: "china", 
             img: "./assets/images/china_guanyu.png" 
         },
         { 
@@ -23,7 +25,8 @@ const CardDatabase = {
             atk: 4500, 
             cost: 2, 
             atkCost: 1, 
-            type: "blocker", 
+            type: "blocker",
+            faction: "china", 
             img: "https://res.cloudinary.com/dju44op8w/image/upload/v1777472457/Gemini_Generated_Image_s7osias7osias7os_fabxbv.jpg" 
         },
         { 
@@ -33,21 +36,24 @@ const CardDatabase = {
             atkCost: 1, 
             type: "warrior",
             counter: 2000, 
-            skills: ["on_play:draw_1"], // When played, draw 1 card
+            skills: ["on_play:draw_1"],
+            faction: "china",
             img: "https://res.cloudinary.com/dju44op8w/image/upload/v1777472501/Gemini_Generated_Image_m8c3vam8c3vam8c3_pqnpqt.jpg" 
         },
         { 
             name: "Meditation", 
             type: "event", 
             effect: "draw_2", 
-            cost: 2, 
+            cost: 2,
+            faction: "china", 
             img: "./assets/images/event_meditation.png" 
         },
         { 
             name: "Divine Elixir", 
             type: "event", 
             effect: "heal_1", 
-            cost: 1, 
+            cost: 1,
+            faction: "china", 
             img: "./assets/images/event_elixir.png" 
         },
         { 
@@ -55,14 +61,16 @@ const CardDatabase = {
             atk: 5000, 
             cost: 3, 
             atkCost: 1, 
-            type: "warrior", 
+            type: "warrior",
+            faction: "china", 
             img: "./assets/images/china_zhubajie.png" 
         },
         { 
             name: "Nuwa's Flood", 
             type: "event", 
             effect: "board_wipe", 
-            cost: 8, 
+            cost: 8,
+            faction: "china", 
             img: "./assets/images/event_nuwa_flood.png" 
         }
     ],
@@ -73,6 +81,7 @@ const CardDatabase = {
             cost: 4, 
             atkCost: 2, 
             type: "warrior",
+            faction: "greek",
             img: "./assets/images/greek_hercules.png" 
         },
         { 
@@ -80,7 +89,8 @@ const CardDatabase = {
             atk: 4000, 
             cost: 2, 
             atkCost: 1, 
-            type: "blocker", 
+            type: "blocker",
+            faction: "greek", 
             img: "./assets/images/greek_spartan.png" 
         },
         { 
@@ -88,21 +98,24 @@ const CardDatabase = {
             atk: 4500, 
             cost: 3, 
             atkCost: 1, 
-            type: "warrior", 
+            type: "warrior",
+            faction: "greek", 
             img: "./assets/images/greek_valkyrie.png" 
         },
         { 
             name: "Oracle's Vision", 
             type: "event", 
             effect: "ai_summon", 
-            cost: 2, 
+            cost: 2,
+            faction: "greek", 
             img: "./assets/images/event_oracle.png" 
         },
         { 
             name: "Ambrosia", 
             type: "event", 
             effect: "heal_1", 
-            cost: 1, 
+            cost: 1,
+            faction: "greek", 
             img: "./assets/images/event_ambrosia.png" 
         },
         { 
@@ -111,14 +124,16 @@ const CardDatabase = {
             cost: 5, 
             atkCost: 2, 
             skills: ["rush"], 
-            type: "warrior", 
+            type: "warrior",
+            faction: "greek", 
             img: "./assets/images/greek_ares.png" 
         },
         { 
             name: "Zeus's Wrath", 
             type: "event", 
             effect: "board_wipe", 
-            cost: 8, 
+            cost: 8,
+            faction: "greek", 
             img: "./assets/images/event_zeus_wrath.png" 
         }
     ]
